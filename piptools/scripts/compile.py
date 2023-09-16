62d621166b9e40f82827e1848e0f02b163239c65
AP Ljungquist <ap@ljungquist.eu>
2023-09-16 11:24:03 +0200

    Make pip-compile silent by default

--- piptools/scripts/compile.py
+++ piptools/scripts/compile.py
@@ -348,7 +348,7 @@ def cli(
                     src_file=src_file,
                     build_distributions=build_deps_for_distributions,
                     isolated=build_isolation,
-                    quiet=log.verbosity < 0,
+                    quiet=log.verbosity <= 0,
                 )
             except BuildBackendException as e:
                 log.error(str(e))
